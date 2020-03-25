72d9eda6381416f176913041ba035e112c0360d7
MalronWall <thibaut.tourte17@gmail.com>
2020-03-25 17:32:19 +0100

    :bug: : Roles array to string

--- src/Domain/Entity/User.php
+++ src/Domain/Entity/User.php
@@ -90,7 +90,7 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        return $this->roles;
+        return $this->roles[0];
     }
 
     public function eraseCredentials()
